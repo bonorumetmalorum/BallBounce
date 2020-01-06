6e4b97460dfc3ff53ec11f6665a6f964ec71161d
Govind <venkateshgovind1@gmail.com>
2020-01-06 23:51:25 +0100

    fixed overlapping scenario

--- ballbounce/CollisionSystem.cpp
+++ ballbounce/CollisionSystem.cpp
@@ -51,37 +51,13 @@ void CollisionSystem::collision(Entity * e1, Entity * e2, std::vector<CollisionS
 			e1->shiftPosition(shift/2.0f);
 			e2->shiftPosition(shift / 2.0f * -1.0f);
 
-			////step1
-			//glm::vec3 vecx = a->getPosition() - b->getPosition();
-			//vecx = glm::normalize(vecx);
-
-			////x dir and y dir
-			//glm::vec3 vecv1 = a->getVelocity() + 0.001f;
-			//float x1 = glm::dot(glm::vec3(1, 0, 0), vecv1);
-			//glm::vec3 vecv1x = vecx * x1;
-			//glm::vec3 vecv1y = vecv1 - vecv1x;
-
-			////same for other sphere
-			//vecx = vecx * -1.0f;
-			//glm::vec3 vecv2 = b->getVelocity( ) + 0.001f;
-			//float x2 = glm::dot(glm::vec3(1, 0, 0), vecv2);
-			//glm::vec3 vecv2x = vecv2 * x2;
-			//glm::vec3 vecv2y = vecv2 - vecv2x;
-
-			//glm::vec3 speeda = 
-			//	vecv1x * (a->getMass() * b->getMass()) / (a->getMass() + b->getMass()) + 
-			//	vecv2x * (2 * b->getMass()) / (a->getMass() + b->getMass()) + vecv1y;
-			//glm::vec3 speedb = 
-			//	vecv1x * (2 * a->getMass()) / (a->getMass() + b->getMass()) + 
-			//	vecv2x * (b->getMass() - a->getMass()) / (a->getMass() + b->getMass()) + vecv2y;
-
 			glm::vec3 speeda = (a->getMass() * a->getVelocity() + b->getMass()*b->getVelocity() + b->getMass() * (a->getCors() * (b->getVelocity() - a->getVelocity()))) / (a->getMass() + b->getMass());
 			glm::vec3 speedb = (a->getMass() * a->getVelocity() + b->getMass()*b->getVelocity() + a->getMass() * (a->getCors() * (a->getVelocity() - b->getVelocity()))) / (a->getMass() + b->getMass());
 
 			CollisionEvent ev1{e1, speedb };
 			CollisionEvent ev2{e2, speeda };
 			sphereEventOUT.push_back(ev1);
-			sphereEventOUT.push_back(ev2);
+				sphereEventOUT.push_back(ev2);
 			return;
 		}
 		else {
@@ -115,11 +91,11 @@ void CollisionSystem::collision(Entity * e1, Entity * e2, std::vector<CollisionS
 
 bool CollisionSystem::spherePlaneCollision(Ball * s, Plane * p, glm::vec3 & displacement)
 {
-	//if (s->getPosition().y <= p->getPosition().y) {
-	//	std::cout << "below plane adjusting y position" << std::endl;
-	//	glm::vec3 newpos = (s->getPosition() + glm::vec3(0, p->getPosition().y + s->getRadius(), 0));
-	//	s->setPosition(newpos);
-	//}
+	if (s->getPosition().y <= p->getPosition().y) {
+		std::cout << "below plane adjusting y position" << std::endl;
+		glm::vec3 newpos = (s->getPosition() + glm::vec3(0, p->getPosition().y + s->getRadius(), 0));
+		s->setPosition(newpos);
+	}
 	glm::vec3 directionToPlane = s->getPosition() - p->getPosition();
 	float projection = glm::dot(p->getNormal(), directionToPlane);
 	if (projection < s->getRadius()) {
@@ -135,6 +111,11 @@ bool CollisionSystem::spherePlaneCollision(Ball * s, Plane * p, glm::vec3 & disp
 bool CollisionSystem::sphereSphereCollision(Ball * a, Ball * b, glm::vec3 & displacement)
 {
 	glm::vec3 c1ToC2 = a->getPosition() - b->getPosition();
+	if (c1ToC2 == glm::vec3(0.0f)) {
+		std::cout << "completely overlapping" << std::endl;
+		displacement = glm::vec3(0.0f, a->getRadius(), 0.0f);
+		return true;
+	}
 	float radiusSum = a->getRadius() + b->getRadius();
 	float distanceBetweenCentres = glm::length(c1ToC2);
 	if (distanceBetweenCentres < radiusSum) {
